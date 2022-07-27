41b9983b8fe6fd7447ae036fc8bfcc9525e1b310
jiaxian-lin <ljx425115084@gmail.com>
2022-07-26 23:18:22 -0700

    add foREST

--- run.py
+++ run.py
@@ -6,11 +6,14 @@ import subprocess
 
 if __name__ == "__main__":
     tool = sys.argv[1]
-    time_limit = 1
+    time_limit = "1"
 
     base_cov_port = 11000
-    services = ["features-service", "languagetool", "ncs", "news", "ocvn", "proxyprint", "restcountries", "scout-api", "scs", "erc20-rest-service", "genome-nexus", "person-controller", "problem-controller", "rest-study", "spring-batch-rest", "spring-boot-sample-app", "user-management", "cwa-verification", "market", "project-tracking-system"]
-    for i in range(20):
+    services = ["features-service", "languagetool", "ncs", "news", "ocvn"]
+    services_2 = ["restcountries", "scout-api", "scs", "genome-nexus", "person-controller"]
+    services_3 = ["problem-controller", "rest-study", "spring-batch-rest", "spring-boot-sample-app", "user-management"]
+    services_4 = ["cwa-verification", "market", "project-tracking-system", "proxyprint"]
+    for i in range(5):
         cov_port = base_cov_port + i*10
         print("Running " + tool + " for " + services[i] + ": " + str(cov_port))
         session = tool + '_' + services[i]
@@ -22,7 +25,7 @@ if __name__ == "__main__":
     time.sleep(int(time_limit) * 60 * 60)
 
     subprocess.run("sh stop_all.sh", shell=True)
-    for i in range(20):
+    for i in range(5):
         subprocess.run("tmux kill-sess -t " + services[i], shell=True)
         subprocess.run("tmux kill-sess -t " + services[i] + "_cov", shell=True)
         subprocess.run("tmux kill-sess -t " + tool + '_' + services[i], shell=True)
